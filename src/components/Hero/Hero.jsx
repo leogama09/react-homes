78f18f93d82c99b15ea14815ee99196424a04778
Leonardo Gama <leogama09@gmail.com>
2023-04-26 17:47:48 -0300

    Animation applied

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Hero.css"
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from "react-countup"
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
@@ -12,7 +13,18 @@ const Hero = () => {
 
                 <div className="hero-title">
                     <div className="orange-circle" />
-                    <h1>Discover <br/>Most Suitable <br/>Property</h1>
+                    <motion.h1
+                        initial={{y: "2rem", opacity: 0}}
+                        animate={{y: 0, opacity: 1}}
+                        transition={{
+                            duration: 2,
+                            type: "spring"
+                        }}
+                    >
+                        Discover <br/>
+                        Most Suitable <br/>
+                        Property
+                    </motion.h1>
                 </div>
 
                 <div className="flexColStart hero-des">
@@ -55,9 +67,16 @@ const Hero = () => {
             </div>
             {/* right side */}
             <div className="flexCenter hero-right">
-                <div className="image-container">
+                <motion.div 
+                    initial={{x: "7rem", opacity: 0}}
+                    animate={{x: 0, opacity: 1}}
+                    transition={{
+                        duration: 2,
+                        type: "spring"
+                    }}
+                    className="image-container">
                     <img src="./hero-image.png" alt="" />
-                </div>
+                </motion.div>
             </div>
         </div>
     </section>
