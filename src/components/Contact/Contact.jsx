a65147118deaf0aa53adcada17c323faf8991813
Leonardo Gama <leogama09@gmail.com>
2023-04-26 18:30:39 -0300

    Project finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,10 +22,10 @@ const Contact = () => {
                             <div className="flexStart">
                                 <div className="flexCenter icon">
                                     <MdCall size={25} />
-                                    <div className="flexColStart detail">
-                                        <span className="primaryText">Call</span>
-                                        <span className="secondaryText">021 123 145 14</span>
-                                    </div>
+                                </div>
+                                <div className="flexColStart detail">
+                                    <span className="primaryText">Call</span>
+                                    <span className="secondaryText">021 123 145 14</span>
                                 </div>
                             </div>
                             <div className="flexCenter button">
@@ -38,11 +38,11 @@ const Contact = () => {
                             <div className="flexStart">
                                 <div className="flexCenter icon">
                                     <BsFillChatDotsFill size={25} />
+                                </div>
                                     <div className="flexColStart detail">
                                         <span className="primaryText">Chat</span>
                                         <span className="secondaryText">021 123 145 14</span>
                                     </div>
-                                </div>
                             </div>
                             <div className="flexCenter button">
                                     Chat Now
@@ -55,10 +55,10 @@ const Contact = () => {
                             <div className="flexStart">
                                 <div className="flexCenter icon">
                                     <BsFillChatDotsFill size={25} />
-                                    <div className="flexColStart detail">
+                                </div>
+                                <div className="flexColStart detail">
                                         <span className="primaryText">Video Call</span>
                                         <span className="secondaryText">021 123 145 14</span>
-                                    </div>
                                 </div>
                             </div>
                             <div className="flexCenter button">
@@ -71,10 +71,10 @@ const Contact = () => {
                             <div className="flexStart">
                                 <div className="flexCenter icon">
                                     <HiChatBubbleBottomCenter size={25} />
-                                    <div className="flexColStart detail">
-                                        <span className="primaryText">Message</span>
-                                        <span className="secondaryText">021 123 145 14</span>
-                                    </div>
+                                </div>
+                                <div className="flexColStart detail">
+                                    <span className="primaryText">Message</span>
+                                    <span className="secondaryText">021 123 145 14</span>
                                 </div>
                             </div>
                             <div className="flexCenter button">
